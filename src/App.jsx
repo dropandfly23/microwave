4947b56a9b39d78e7aa71b8298de903192a14de2
dropandfly23 <48749827+dropandfly23@users.noreply.github.com>
2025-07-18 21:31:17 +0200

    Init - react app for microwave slot reservation in my workplace

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
           <motion.div
               animate={{ rotate: 360 }}
               transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
-              className="w-8 h-8 border-4 border-blue-600 border-t-transparent rounded-full"
+              className="w-8 h-8 border -4 border-blue-600 border-t-transparent rounded-full"
           />
         </div>
     );
